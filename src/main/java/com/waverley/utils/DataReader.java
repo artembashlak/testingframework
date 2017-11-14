f19ba776a840f3e907261e8c669f103ca4ee37e5
abashak <bashlak@admins-MacBook-Pro-6.local>
2017-11-14 23:34:00 +0200

    refactor and create utils classes for data reader

--- src/main/java/com/waverley/utils/DataReader.java
+++ src/main/java/com/waverley/utils/DataReader.java
@@ -1,4 +1,9 @@
 package com.waverley.utils;
 
-public class DataReader {
-}
+/**
+ * Interface that will perform read from any data file source.
+ */
+
+public interface DataReader {
+         <T> T read(String dataSource, Class<T> entityClass);
+ }
--- src/main/java/com/waverley/utils/XmlUtils.java
+++ src/main/java/com/waverley/utils/XmlUtils.java
@@ -1,4 +1,22 @@
 package com.waverley.utils;
 
-public class XmlUtils {
+import io.vavr.control.Try;
+import javax.xml.bind.JAXBContext;
+import java.io.File;
+import static java.lang.ClassLoader.getSystemResource;
+import static javax.xml.bind.JAXBContext.newInstance;
+
+/**
+ *  A simple class for reading Json files.
+ */
+
+public class XmlUtils implements DataReader {
+
+    @Override
+    public <T> T read(String dataSource, Class<T> entityClass) {
+        return Try.of(() -> newInstance(entityClass))
+                .mapTry(JAXBContext::createUnmarshaller)
+                .mapTry(unmarshaller -> (T) unmarshaller.unmarshal(new File(getSystemResource(dataSource).getPath())))
+                .getOrElseThrow(e -> new IllegalArgumentException("Unable to read " + dataSource, e));
+    }
 }
