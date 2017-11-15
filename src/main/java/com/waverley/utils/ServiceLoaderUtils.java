609c6ade35e5c4b74db6731a1e32bbe8cbc8cd6a
abashak <bashlak@admins-MacBook-Pro-6.local>
2017-11-15 02:40:58 +0200

    Change codestyle issues

--- src/main/java/com/waverley/utils/ServiceLoaderUtils.java
+++ src/main/java/com/waverley/utils/ServiceLoaderUtils.java
@@ -14,7 +14,7 @@ public class ServiceLoaderUtils {
         throw new UnsupportedOperationException("Illegal access to private constructor.");
     }
 
-    public static <T> List<T> load (final Class<T> type, final ClassLoader classLoader ) {
+    public static <T> List<T> load(final Class<T> type, final ClassLoader classLoader) {
         return Try.of(() -> StreamEx.of(ServiceLoader.load(type, classLoader).iterator()).toList())
                 .getOrElseGet(ex -> Collections.emptyList());
     }
